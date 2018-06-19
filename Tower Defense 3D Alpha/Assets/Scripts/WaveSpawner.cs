ee00a64ca029f430e497607efb3d68c41b7d6e70
Gustavo-Martins <gustavo737@gmail.com>
2018-06-19 11:01:28 -0300

    Adds enemies wave spawn

---
+++ Tower Defense 3D Alpha/Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class WaveSpawner : MonoBehaviour {
+
+    public Transform enemyPrefab;
+
+    public Transform spawnPoint;
+
+    private float countdown = 2f;
+    private float startWait = 0.5f;
+    private float spawnWait = 0.5f;
+    public float timeBetweenWaves = 5f;
+
+    private int waveIndex = 0;
+
+    void Update()
+    {
+        if (countdown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves + startWait * waveIndex;
+            Debug.Log("Starting wave:" + waveIndex);
+        }
+
+        countdown -= Time.deltaTime;
+    }
+
+    IEnumerator SpawnWave()
+    {
+        for (int i = 0; i < waveIndex; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(spawnWait);
+        }
+
+        waveIndex++;
+
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
