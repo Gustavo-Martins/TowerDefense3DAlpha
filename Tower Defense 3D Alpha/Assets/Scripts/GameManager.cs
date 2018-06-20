dba75a2841179b3355b9966fec0140d6e74bcd62
Gustavo-Martins <gustavo737@gmail.com>
2018-06-20 19:36:30 -0300

    Adds Standard Turret building

---
+++ Tower Defense 3D Alpha/Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    // Singleton pattern
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("More than one GameManager in scene!");
+            return;
+        }
+        instance = this;
+    }
+
+    public GameObject standardTurretPrefab;
+
+    void Start()
+    {
+        turretToBuild = standardTurretPrefab;
+    }
+
+    private GameObject turretToBuild;
+
+    public GameObject GetTurretToBuild()
+    {
+        return turretToBuild;
+    }
+
+}
---
+++ Tower Defense 3D Alpha/Assets/Scripts/Node.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class Node : MonoBehaviour {
+
+    public Color hoverColor;
+    public Color startColor;
+
+    private GameObject turret;
+
+    private Renderer rend;
+    public Vector3 positionOffSet;
+    
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    void OnMouseDown()
+    {
+        if (turret != null)
+        {
+            Debug.Log("You can't build there!");
+            return;
+        }
+
+        GameObject turretToBuild = GameManager.instance.GetTurretToBuild();
+        turret = (GameObject)Instantiate(turretToBuild, transform.position + positionOffSet, transform.rotation);
+
+    }
+       
+    void OnMouseEnter()
+    {
+        GetComponent<Renderer>().material.color = hoverColor;
+    }
+
+    void OnMouseExit()
+    {
+        rend.material.color = startColor;
+    }
+}
